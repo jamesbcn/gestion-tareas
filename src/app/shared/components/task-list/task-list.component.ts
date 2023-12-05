98ab8567ae444697745820ee2025440d40a0a114
jamesbcn <jamesquinlan@hotmail.co.uk>
2023-12-05 22:25:47 +0100

    FormControl filtro

--- src/app/shared/components/task-list/task-list.component.ts
+++ src/app/shared/components/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, map, of, tap } from 'rxjs';
 import { Task } from '../../../models/task.model';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Observable } from 'rxjs';
 import { TaskService } from '../../services/task.service';
 import { TaskModifyComponent } from '../task-modify/task-modify.component';
@@ -10,12 +10,14 @@ import { DeepCopyService } from '../../services/deep-copy.service';
 import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [NgFor, AsyncPipe, MatButtonModule, MatIconModule, FormsModule],
+  imports: [NgFor, AsyncPipe, MatButtonModule, MatIconModule, MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, JsonPipe],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.sass'
 })
@@ -24,25 +26,34 @@ export class TaskListComponent implements OnInit, OnDestroy {
   
   tasks$!: Observable<Task[]>;
 
-  tags: string[] = [];
-  selectedTags: string[] = [];
+  tagsSelected = new FormControl('');
+  tagsList: string[] = [];
 
   private taskModifiedSubscription: Subscription = new Subscription();
 
   constructor(private taskService: TaskService, private dialog: MatDialog, private copyService: DeepCopyService) {}
 
   ngOnInit(): void {
+
+    this.tagsSelected.valueChanges.subscribe(
+      (values:any): void => {
+
+        let arr = [...values]
+
+        this.filterTasksByTags(arr);
+        
+        }
+    );
     
     this.tasks$ = this.taskService.getAllTasks().pipe(
 
       map(tasks => {
-        // Side Effect: create list of all tags available;
-        const tags = tasks.flatMap(task => task.tags.map(tag => tag.name));
+        // Side Effect: crear un listado de todas las etiquetas disponibles
+        const tagsAll = tasks.flatMap(task => task.tags.map(tag => tag.name));
 
-        // Quitar duplicados;
-        this.tags = [...new Set(tags)];
+        // Quitar los duplicados
+        this.tagsList = [...new Set(tagsAll)];
         
-        console.log(this.tags);
         return tasks;
       })
       
@@ -54,7 +65,7 @@ export class TaskListComponent implements OnInit, OnDestroy {
     this.taskModifiedSubscription = this.taskService.taskModified$().subscribe(
       (modifiedTask) => {
         // Manejar el evento de modificación de la tarea
-        console.log('Task modified event received:', modifiedTask);
+        console.log('Evento de tarea modificado se ha recibido:', modifiedTask);
 
         // Actualizar la tarea única en el observable tasks$
         this.tasks$ = this.tasks$.pipe(
@@ -67,6 +78,7 @@ export class TaskListComponent implements OnInit, OnDestroy {
   }
 
   filterTasksByTags(tags: string[]): void {
+
     // Update the tasks$ observable based on selected tags
     this.tasks$ = this.taskService.getAllTasks().pipe(
       map(tasks => tasks.filter(task => tags.every(tag => task.tags.some(taskTag => taskTag.name === tag))))
