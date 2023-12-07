98ca41e60212df63bc68f6619f68720ca17f0999
jamesbcn <jamesquinlan@hotmail.co.uk>
2023-12-07 15:09:40 +0100

    interceptor logout

--- src/app/auth/jwt.interceptor.ts
+++ src/app/auth/jwt.interceptor.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -18,7 +20,18 @@ export class JwtInterceptor implements HttpInterceptor {
         },
       });
 
-      return next.handle(cloned);
+      return next.handle(cloned).pipe(
+        catchError((error: HttpErrorResponse) => {
+
+          console.error(error)
+          if (error.status === 401) {
+            // Token exp.
+            this.authService.logout(); 
+            this.router.navigate(['/login']); 
+          }
+          return throwError(() => error);
+        })
+      );
     } else {
       return next.handle(request);
     }
