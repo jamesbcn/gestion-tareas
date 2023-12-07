80a4a059868d105700419dc5aff8ac972ae1744f
jamesbcn <jamesquinlan@hotmail.co.uk>
2023-12-07 12:10:54 +0100

    environment variable

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable, shareReplay, tap  } from 'rxjs';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +12,13 @@ export class AuthService {
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.checkLocalStorageAuth());
   public isAuthenticated$: Observable<boolean> = this.isAuthenticatedSubject.asObservable();
 
+  private baseUrl = environment.domain;
+
   constructor(private http: HttpClient, private router: Router) {}
 
   login(username:string, password:string): Observable<User> {
 
-    return this.http.post<User>("http://localhost:9000/api/login", {username, password})
+    return this.http.post<User>(`${this.baseUrl}/login`, {username, password})
         .pipe(
             tap(username => {
                 this.isAuthenticatedSubject.next(true);
